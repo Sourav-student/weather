4701466732d83517d2d56bee8afea8a52e4a57d8
Coding With Sourav <so34ur56@gmail.com>
2024-11-21 23:51:58 +0530

    updated js

--- script.js
+++ script.js
@@ -35,10 +35,24 @@ async function weather(city) {
         locationCity.innerHTML = data.name;
         area.innerHTML = data.sys.country;
 
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-        weatherImage.innerHTML = `<img src= ${iconUrl} alt="weatherImage" class = "img1">`;
+        //Showing images according to the weather
         conditionWeather.innerHTML = data.weather[0].main;
+        
+        if (conditionWeather.innerText == "Smoke") {
+            weatherImage.innerHTML = `<img src="smoke.png" alt="weatherImage" class = "img1">`;
+        }
+
+        else if (conditionWeather.innerText == "Rain" ) {
+            weatherImage.innerHTML = `<img src="rain.png" alt="weatherImage" class = "img1">`;
+        }
+
+        else if (conditionWeather.innerText == "Mist" ) {
+            weatherImage.innerHTML = `<img src="mist.png" alt="weatherImage" class = "img1">`;
+        }
+
+        else if (conditionWeather.innerText == "Clouds" ) {
+            weatherImage.innerHTML = `<img src="cloud.png" alt="weatherImage" class = "img1">`;
+        }
 
         //Main 
         temp.innerHTML = Math.round(data.main.temp - 273.15);
@@ -69,20 +83,30 @@ async function weather(city) {
             // Update background based on the hour
             if (hour >= 0 && hour < 24) {
                 if (hour < 4) {
-                    main.style.backgroundImage = "url(istockphoto-162515751-612x612.jpg)";
-                    weatherImage.style.filter = "invert(1)";
+                    main.style.backgroundImage = "url(istockphoto-162515751-612x612.png)";
+                    if (conditionWeather.innerText == "Clear" ) {
+                        weatherImage.innerHTML = `<img src="clear.png" alt="weatherImage" class = "img1">`;
+                    }
                 } else if (hour < 12) {
                     main.style.backgroundImage = "url(istockphoto-516180836-612x612.jpg)";
-                    weatherImage.style.filter = "";
+                    if (conditionWeather.innerText == "Clear" ) {
+                        weatherImage.innerHTML = `<img src="sunny.png" alt="weatherImage" class = "img1">`;
+                    }
                 } else if (hour < 16) {
                     main.style.backgroundImage = "url(istockphoto-917178010-612x612.jpg)";
-                    weatherImage.style.filter = "";
+                    if (conditionWeather.innerText == "Clear" ) {
+                        weatherImage.innerHTML = `<img src="sunny.png" alt="weatherImage" class = "img1">`;
+                    }
                 } else if (hour < 21) {
                     main.style.backgroundImage = "url(evening-7432219_1280.jpg)";
-                    weatherImage.style.filter = "invert(0.5)";
+                    if (conditionWeather.innerText == "Clear" ) {
+                        weatherImage.innerHTML = `<img src="clear.png" alt="weatherImage" class = "img1">`;
+                    }
                 } else {
-                    main.style.backgroundImage = "url(istockphoto-162515751-612x612.jpg)";
-                    weatherImage.style.filter = "invert(1)";
+                    main.style.backgroundImage = "url(istockphoto-162515751-612x612.png)";
+                    if (conditionWeather.innerText == "Clear" ) {
+                        weatherImage.innerHTML = `<img src="clear.png" alt="weatherImage" class = "img1">`;
+                    }
                 }
             } 
         } 
@@ -112,7 +136,6 @@ function showWeather() {
             weather(city); // Call the weather function with the city input
         }
     });
-
 }
 
 showWeather();
